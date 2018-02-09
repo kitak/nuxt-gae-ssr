3f4ea5ddf26ec246829ee3100c474d7d26712875
Keisuke KITA <kei.kita2501@gmail.com>
2018-02-09 16:30:36 +0900

    Create builder when dev mode is enabled

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const { Nuxt } = require('nuxt');
+const { Nuxt, Builder } = require('nuxt');
 const app = require('express')();
 const host = process.env.HOST || '0.0.0.0';
 const port = process.env.PORT || 3000;
@@ -7,13 +7,17 @@ app.set('port', port);
 
 const config = require('./nuxt.config.js');
 config.dev = !(process.env.NODE_ENV === 'production');
+const nuxt = new Nuxt(config);
+
+if (config.dev) {
+  new Builder(nuxt).build();
+}
 
 app.get('/_ah/health', (req, res) => {
   res.status(200);
   res.send();
 });
 
-const nuxt = new Nuxt(config);
 app.use(nuxt.render);
 
 app.listen(port, host);
